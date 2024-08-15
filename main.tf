a13c713a78ddaf3bbfc9a99e67ec894d4ea8bf02
geeta <geetarekhadi@gmail.com>
2024-08-15 09:39:07 +0530

    terraform_project

--- main.tf
+++ main.tf
@@ -1,76 +1,168 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "myterraformVPC"
-  }
+resource "aws_subnet" "sub1" {
+  vpc_id                          = aws_vpc.myvpc.id
+  cidr_block                      = "10.0.0.0/24"
+  availability_zone               = "us-east-1a"
+  
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
+resource "aws_subnet" "sub2" {
+  vpc_id                          = aws_vpc.myvpc.id
+  cidr_block                      = "10.0.1.0/24"
+  availability_zone               = "us-east-1b"
+  
 }
 
-resource "aws_subnet" "web" {
-  count = 3
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.${count.index}.0/24"
-  availability_zone = "us-east-1a"  // Adjust as per your region
-}
-
-resource "aws_subnet" "tomcat" {
-  count = 3
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.${count.index + 10}.0/24"
-  availability_zone = "us-east-1b"  // Adjust as per your region
-}
-
-resource "aws_subnet" "alb" {
-  count = 3
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.${count.index + 20}.0/24"
-  availability_zone = "us-east-1c"  // Adjust as per your region
-}
-
-resource "aws_subnet" "jump" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.30.0/24"
-  availability_zone = "us-east-1a"  // Adjust as per your region
-}
-resource "aws_subnet" "publicSubnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-}
-
-resource "aws_subnet" "privateSubnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.2.0/24"
-}
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.myvpc.id
 }
-resource "aws_route_table" "PublicRT" {
-  vpc_id = aws_vpc.my_vpc.id
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 }
-resource "aws_route_table_association" "PublicRTassociation" {
-  subnet_id      = aws_subnet.privateSubnet.id
-  route_table_id = aws_route_table.PublicRT.id
 
+resource "aws_route_table_association" "RTa1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
 }
 
-resource "aws_instance" "EC2" {
-  ami           = "ami-0ba9883b710b05ac6"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.publicSubnet.id  # Correct subnet reference
+resource "aws_route_table_association" "RTa2" {
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "mysg" {
+  name_prefix = "websg"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "my_demo_instance"
+    Name = "web-sg"
   }
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "terraform.s3.geeta"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = "terraform.s3.geeta"
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
+}
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-04a81a99f5ec58529"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.mysg.id]
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file("userdata.sh"))
+}
+
+resource "aws_instance" "webserver2" {
+  ami                    = "ami-04a81a99f5ec58529"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.mysg.id]
+  subnet_id              = aws_subnet.sub2.id
+  user_data              = base64encode(file("userdata1.sh"))
+}
+
+#create alb
+resource "aws_lb" "myalb" {
+  name               = "myalb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.mysg.id]
+  subnets         = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+
+  tags = {
+    Name = "web"
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "myTG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.tg.arn
+    type             = "forward"
+  }
+}
+
+output "loadbalancerdns" {
+  value = aws_lb.myalb.dns_name
 }
